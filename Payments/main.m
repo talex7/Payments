63c363c8d3fb6b4241028d28985310a5cb549d41
talex7 <talexanian@gmail.com>
2016-11-06 18:12:31 -0500

    Initial Commit

---
+++ Payments/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Payments
+//
+//  Created by Thomas Alexanian on 2016-11-06.
+//  Copyright © 2016 Thomas Alexanian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
