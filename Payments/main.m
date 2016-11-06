d3fe4d8535430396791569deada78accb57892e3
talex7 <talexanian@gmail.com>
2016-11-06 18:31:09 -0500

    i/o in main added

--- Payments/main.m
+++ Payments/main.m
@@ -10,8 +10,23 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+       
+        int cost = arc4random_uniform(900) + 100;
+    
+        NSLog (@"Thank you for shopping at Acme.com Your total today is $%d Please select your payment method: 1: Paypal, 2: Stripe, 3: Amazon", cost);
+        
+        char answer[255];
+        
+        NSCharacterSet *charSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+        
+        fgets(answer, 256, stdin);
+        
+        NSString *result = [NSString stringWithCString:answer encoding:NSUTF8StringEncoding];
+        
+       int choice = [[result stringByTrimmingCharactersInSet:charSet] intValue];
+        
+        NSLog(@"%d", choice);
+        
     }
     return 0;
 }
